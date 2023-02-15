6689e5724c615c86445e3f8a3fd457c20d991731
luddebassen <luddebassen213@gmail.com>
2023-02-15 12:47:59 +0100

    background color on native program

--- Main/src/app.rs
+++ Main/src/app.rs
@@ -1,7 +1,8 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 
-use eframe::egui;
+use eframe::{egui, glow::{FILL, BLUE}};
 use egui_extras::image::FitTo;
+use egui::Color32;
 
 
 pub struct SvgApp {
@@ -25,7 +26,17 @@ impl Default for SvgApp {
 //design of the app, look at documenation for inspiration
 impl eframe::App for SvgApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        egui::CentralPanel::default().show(ctx, |ui| {
+        //design the frame
+        let my_frame = egui::containers::Frame {
+            inner_margin: egui::style::Margin { left: 0., right: 0., top: 0., bottom: 0. }, //margins (affects the color-border)
+            outer_margin: egui::style::Margin { left: 0., right: 0., top: 0., bottom: 0. },
+            rounding: egui::Rounding { nw: 1.0, ne: 1.0, sw: 1.0, se: 1.0 },
+            shadow: eframe::epaint::Shadow { extrusion: 1.0, color: Color32::YELLOW },
+            fill: Color32::WHITE, //background fill color, affected by the margin
+            stroke: egui::Stroke::new(2.0, Color32::GOLD),
+        };
+        //ui panels
+        egui::CentralPanel::default().frame(my_frame).show(ctx, |ui| {
             ui.heading("SVG example");
             ui.label("The SVG is rasterized and displayed as a texture.");
 
