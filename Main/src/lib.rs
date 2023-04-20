bca7adf9a59714d4ec0e7d4a7935bec7eb2a3355
luddebassen <luddebassen213@gmail.com>
2023-04-20 17:07:38 +0200

    Fixed cargo structure

--- Main/src/lib.rs
+++ Main/src/lib.rs
@@ -1,1 +1,6 @@
 #![warn(clippy::all, rust_2018_idioms)]
+
+pub mod algorithms;
+mod dxfextract;
+mod dxfwrite;
+mod svgwrite;
--- Main/tests/integration_test.rs
+++ Main/tests/integration_test.rs
@@ -1,8 +1,10 @@
+#[cfg(test)]
 
-
+use dxf_janitors;
 //Integration tests => test public functions that are in use in other modules
 //Connect and Extend is from algorithm.rs used by main.rs
+
 #[test]
-fn test_try_to_connect_polylines(){
+fn test_try_to_connect_polylines(){    
     
 }
