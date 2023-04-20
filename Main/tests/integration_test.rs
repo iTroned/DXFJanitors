7479ec8ba31c933f0283c5a86209c314909e8ee8
luddebassen <luddebassen213@gmail.com>
2023-04-20 14:31:09 +0200

    Tests folder + integration test start

---
+++ Main/tests/integration_test.rs
@@ -0,0 +1,8 @@
+
+
+//Integration tests => test public functions that are in use in other modules
+//Connect and Extend is from algorithm.rs used by main.rs
+#[test]
+fn test_try_to_connect_polylines(){
+    
+}
